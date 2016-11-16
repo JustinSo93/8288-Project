cb4f7a1ae04f9e88c9b78cca2f3f87b24747ab95
JustinSo93 <justinso93@hotmail.com>
2016-11-16 10:48:53 -0500

    Add files via upload

---
+++ Movement.java
@@ -0,0 +1,29 @@
+package domain;
+
+public class Movement {
+
+	private int id;
+	private String movementName;
+	
+	public int getId(){
+		return id;
+		
+	}
+	
+	public void setId(int id){
+		
+	}
+	
+	public String getMovementName(){
+		return movementName;
+		
+	}
+	
+	public void setMovementName(String movementName){
+		
+	}
+	
+	protected void operation(){
+		
+	}
+}
