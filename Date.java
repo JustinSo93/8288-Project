2f47bccc936d2ca5e871d3b8a33e7467e905cf81
JustinSo93 <justinso93@hotmail.com>
2016-11-16 18:55:47 -0500

    Update Date.java

--- Date.java
+++ Date.java
@@ -7,74 +7,79 @@ private int month;
 private int year;
 	
 	
+	public Date (){
+		this.day =0;
+		this.month = 0;
+		this.year = 0;
+	}
 	
-	public Date(int x, int y, int z) {
-		
-		this.day = x;
-		this.month = y;
-		this. year = z;
-	// TODO Auto-generated constructor stub
-}
-
-
-
+	public Date(int d, int m, int y){
+		this.day = d;
+		this.month = m;
+		this.year = y;
+	}
+		/**
+	 * @return the day
+	 */
+	public int getDay() {
+		return day;
+	}
+	
+	
+	
 	/**
- * @return the day
- */
-public int getDay() {
-	return day;
-}
+	 * @param day the day to set
+	 */
+	public void setDay(int day) {
+		this.day = day;
+	}
+	
+	
+	
+	/**
+	 * @return the month
+	 */
+	public int getMonth() {
+		return month;
+	}
+	
+	
+	
+	/**
+	 * @param month the month to set
+	 */
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	
+	
+	
+	/**
+	 * @return the year
+	 */
+	public int getYear() {
+		return year;
+	}
+	
+	
+	
+	/**
+	 * @param year the year to set
+	 */
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	public String toString(){
+		return day+"//"+month+"//"+year;
+	}
+	
 
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-/**
- * @param day the day to set
- */
-public void setDay(int day) {
-	this.day = day;
-}
-
-
-
-/**
- * @return the month
- */
-public int getMonth() {
-	return month;
-}
-
-
-
-/**
- * @param month the month to set
- */
-public void setMonth(int month) {
-	this.month = month;
-}
-
-
-
-/**
- * @return the year
- */
-public int getYear() {
-	return year;
-}
-
-
-
-/**
- * @param year the year to set
- */
-public void setYear(int year) {
-	this.year = year;
-}
-
-public String toString(){
-	return day+"//"+month+"//"+year;
-}
-
-
-
+	}
 
 }
+
