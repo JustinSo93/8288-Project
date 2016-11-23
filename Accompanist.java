f9bf6b2e925cc3107878a95da407bee016239684
TylerWoyiwada <woyi0001@algonquinlive.com>
2016-11-23 11:44:20 -0500

    Add files via upload
    now with javadocs

--- Accompanist.java
+++ Accompanist.java
@@ -2,12 +2,35 @@ package domain;
 
 import java.util.ArrayList;
 
+/**
+ * This Subclass of Person creates an accompanist with extra attributes.
+ * @author Tyler Woyiwada
+ *
+ */
+
 public class Accompanist extends Person{
+
+/**
+ * Additional attributes-
+ * 
+ * @param ID accompID- generated ID for the accompanist.  
+ * @param String instrumentGroup- the instrument group of the instrument that the accompanist play.
+ * @param String instrumentName- the instrument that the accompanist plays.
+ */
 	ID accompID;
 	String instrumentGroup;
 	String instrumentName;
 
-	
+/** Constructor.
+ * 
+ * @param firstName
+ * @param lastName
+ * @param phone
+ * @param address
+ * @param emailAddress
+ * @param instrument
+ * @param instrumentGroup
+ */
 Accompanist(String firstName, String lastName, PhoneNumber phone, 
 		Address address, String emailAddress, String instrument, String instrumentGroup){
 	
@@ -16,36 +39,55 @@ Accompanist(String firstName, String lastName, PhoneNumber phone,
 	this.instrumentGroup =instrumentGroup;
 	this.setAccompID();
 	}
-	
+/** public get method for accompID
+ * 
+ */
     public ID getAccompID() {
 		return accompID;
 	}
 
-
+/** public set method for accompID
+ * 
+ */
 	public void setAccompID() {
 		 accompID = IDFactory.getID("A");
 	}
 
-
+/** public get method for instrumentGroup
+ * 
+ * @return String instrumentGroup
+ */
 	public String getInstrumentGroup() {
 		return instrumentGroup;
 	}
-
+/**
+ * public set method for instrumentGroup.
+ * @param instrumentGroup
+ */
 
 	public void setInstrumentGroup(String instrumentGroup) {
 		this.instrumentGroup = instrumentGroup;
 	}
 
-
+/**
+ * public get method for instrumentName.
+ * @return instrumentName
+ */
 	public String getInstrumentName() {
 		return instrumentName;
 	}
-
+/**
+ * public set method for instrumentName
+ * @param instrumentName
+ */
 
 	public void setInstrumentName(String instrumentName) {
 		this.instrumentName = instrumentName;
 	}
-
+/**
+ * public toString method returns a String value of Accompanist.
+ * @return Accompanist.toString
+ */
 	@Override
 	public String toString() {
 		
@@ -53,5 +95,10 @@ Accompanist(String firstName, String lastName, PhoneNumber phone,
 				+ ", \nphoneNumber: " + phoneNumber + ", instrument group: " + instrumentGroup
 				+ ", instrument name: " + instrumentName + "]\n*****************************************************\n ";
 	}
+	
+	/**
+	 * end of class.
+	 */
+	 
 	
 }
