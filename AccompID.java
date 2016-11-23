4fa921eb4b7d49a61a42ac54f91ea0715b9dc9df
TylerWoyiwada <woyi0001@algonquinlive.com>
2016-11-23 11:54:47 -0500

    Add files via upload

--- AccompID.java
+++ AccompID.java
@@ -1,20 +1,31 @@
 package domain;
 
-
+/**
+ * Subclass of IDFactory that will auto generate an ID key for an accompanist.  
+ * @author tyyyl
+ *
+ */
 public class AccompID implements ID{
-
+/**Variables that form the ID Key.
+ * @param public int musCount
+ * @param public String musCode
+ * 
+ */
 	public static int musCount;
-	
-	
 	String musCode = "ACC";
-
-
-
+/**
+ * get method for the musCode
+ * @return musCode.toString
+ * 
+ */
 	@Override
 	public String createID() {
 				return musCode.toString();
 				}
-	
+/**
+ * toString method for the ID code.
+ * @return musCount + musCode toString.
+ */
 	@Override 
 	public String toString(){
 		musCount ++;
