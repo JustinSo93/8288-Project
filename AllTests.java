cce604a5d53aa4069b085af2e7110005ead4c535
Jessica Stratton <stra0157@algonquinlive.com>
2016-11-25 11:28:29 -0500

    Add files via upload

--- AllTests.java
+++ AllTests.java
@@ -12,11 +12,23 @@ public class AllTests extends TestCase {
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite();
-		//suite.addTest(Test_Venues.suite());
+		suite.addTest(Test_Venues.suite());
 		suite.addTest(Test_Address.suite());
 		suite.addTest(Test_PhoneNumber.suite());
 		suite.addTest(Test_Tickets.suite());
 		suite.addTest(Test_AccompID.suite());
+		suite.addTest(Test_AccompID.suite());
+		suite.addTest(Test_ConductorID.suite());
+		suite.addTest(Test_Customer.suite());
+		suite.addTest(Test_CustomerID.suite());
+		suite.addTest(Test_Date.suite());
+		suite.addTest(Test_IDFactory.suite());
+		suite.addTest(Test_StaffID.suite());
+		suite.addTest(Test_TicketID.suite());
+		suite.addTest(Test_TicketModel.suite());
+		suite.addTest(Test_TransactionID.suite());
+		suite.addTest(Test_VenueID.suite());
+		
 		return suite;
 	}
 
