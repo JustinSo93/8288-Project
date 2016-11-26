a43ba639de213e3b1c095eccfcfb6ffa5d2cae46
niravBaldha <niravbaldha007@gmail.com>
2016-11-26 15:02:55 -0500

    Add files via upload

--- StaffID.java
+++ StaffID.java
@@ -1,16 +1,32 @@
 package domain;
-
+/**
+ * StaffID class implements ID interface.
+ * this class is used for create unique ID for new staff member.
+ * Example STA1, STA2, STA3 
+ * @author Nirav
+ *
+ */
 public class StaffID implements ID {
 
-
+			/**
+			 * @param staffCount type of static int with default value "0"
+			 */
 			public static int staffCount = 0;
 				String staffCode = "STA";
 
+				/**
+				 * override method CreateID(),
+				 *@return toString of staffCode
+				 */
 				@Override
 				public String createID() {
 							
 							return staffCode.toString();
 				}
+				/**
+				 * override method of toString(),
+				 * @return staffCode+ staffCount as string.
+				 */
 				@Override 
 				public String toString(){
 					staffCount++;
