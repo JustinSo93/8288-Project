29e46ee4d9646faf5a53f2209c0fb2aa2625aec4
JustinSo93 <justinso93@hotmail.com>
2016-11-26 13:16:02 -0500

    Add files via upload

--- Soloist.java
+++ Soloist.java
@@ -4,11 +4,17 @@ import java.util.ArrayList;
 
 public class Soloist extends Person {
 	
-	ArrayList<CompLastDatePlayed> compositionsDate;
-	ID soloistID;
-	String instrumentName;
-	String instrumentGroup;
 	
+	/* CONSTRUCTORS -------------------------------------------------- */
+	
+	/** Empty constructor */
+	public Soloist(){
+		super();
+		this.instrumentName = "";
+		this.instrumentGroup = "";
+		this.setSoloistID();
+	}
+	/** Fully parameterized constructor */
 	public Soloist(String firstName, String lastName, PhoneNumber phone, Address address, String emailAddress, String instrument,
 			String instrumentGroup, ArrayList<CompLastDatePlayed> compositionsDate) {
 		super(firstName, lastName, phone, address, emailAddress);
@@ -20,45 +26,74 @@ public class Soloist extends Person {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public Soloist(){
-		super();
-		this.instrumentName = "";
-		this.instrumentGroup = "";
-		this.setSoloistID();
-	}
+	/* ACCESSORS ----------------------------------------------------- */
+	
+	/**
+	 *  This method returns the soloistID 
+	 * @return soloistID
+	 */
 	public ID getSoloistID() {
-		
 		return soloistID;
 	}
-
-
+	/**
+	 * This method returns the instrumentGroup
+	 * @return instrumentGroup
+	 */
+	public String getInstrumentGroup() {
+		return instrumentGroup;
+	}
+	
+	/**
+	 * This method returns the instrumentName
+	 * @return instrumentName
+	 */
+	public String getInstrumentName() {
+		return instrumentName;
+	}
+	
+	/* MODIFIERS ----------------------------------------------------- */
+	/**
+	 * This void method sets the soloistID
+	 */
 	public void setSoloistID() {
 		 soloistID = IDFactory.getID("S");
 	}
 
-	public String getInstrumentGroup() {
-		return instrumentGroup;
-	}
-
-
+	/**
+	 * This void method sets the instrumentGroup
+	 * @param instrumentGroup
+	 */
 	public void setInstrumentGroup(String instrumentGroup) {
 		this.instrumentGroup = instrumentGroup;
 	}
-
-
-	public String getInstrumentName() {
-		return instrumentName;
-	}
-
-
+	
+	/**
+	 * This void method sets the instrumentName
+	 * @param instrumentName
+	 */
 	public void setInstrumentName(String instrumentName) {
 		this.instrumentName = instrumentName;
 	}
+	/* HELPER METHODS	--------------------------------------------------	*/
+	/**
+	 * This method returns a String and override the Object toString() method. Prints out all relevant information
+	 * concerning the Soloist
+	 */
 	@Override
 	public String toString() {
-	return "\nID : " + this.soloistID.toString() + ", name: " + firstName + " " + lastName + ", \naddress: " + address.toString()
-	+ ", \nphone number: " + phoneNumber  + ", \ninstrument group: " + instrumentGroup
-	+ ", instrument name: " + instrumentName + "\nCompositions and last date played: " + compositionsDate.toString() +
-	"\n***************************************************** \n ";
-}
+		return "\nID : " + this.soloistID.toString() + ", name: " + firstName + " " + lastName + ", \naddress: " + address.toString()
+		+ ", \nphone number: " + phoneNumber  + ", \ninstrument group: " + instrumentGroup
+		+ ", instrument name: " + instrumentName + "\nCompositions and last date played: " + compositionsDate.toString() +
+		"\n***************************************************** \n ";
+	}
+	
+	/* ATTRIBUTES ----------------------------------------------------- */
+	/** compositionsDate is of type ArrayList<CompLastDatePlayed>*/
+	ArrayList<CompLastDatePlayed> compositionsDate;
+	/** soloistID is a unique identifier for each soloist. It is of type ID */
+	ID soloistID;
+	/** Attribute instrumentName is of type String */
+	String instrumentName;
+	/** Attribute instrumentGroup is of type String */
+	String instrumentGroup;
 }
