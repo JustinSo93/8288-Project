36dea77eae9a4c4eb11b5fc08462e84090bc438c
TylerWoyiwada <woyi0001@algonquinlive.com>
2016-11-26 13:43:46 -0500

    Add files via upload
    Added javadocs

--- FinancialTransaction.java
+++ FinancialTransaction.java
@@ -1,8 +1,20 @@
 package domain;
 
 import java.util.ArrayList;
+/**This class will create a financial transaction object with various parameters
+ *
+ * @author Tyler Woyiwada, Justin So
+*/	 
 
 public abstract class FinancialTransaction {
+	
+	/**attributes to be used within the class
+	 * @param currency
+	 * @param sC
+	 * @param amount
+	 * @param source
+	 * @param destination
+	 */
 	protected ArrayList<Tickets> t;
 	protected String currency;
 	protected ScheduledConcert sC;
@@ -11,24 +23,39 @@ public abstract class FinancialTransaction {
 	protected Object source;
 	protected Object destination;
 	
+	/**
+	 * empty constructor
+	 */
 	public FinancialTransaction(){
 		this.currency = "";
 		this.amount = 0;
 		this.source = null;
 		this.destination = null;
 		this.sC = null;
-		this.setTransactionID();
 	}
-	
+	/**
+	 * constructor
+	 * 
+	 * @param currency
+	 * @param sC
+	 * @param amount
+	 * @param source
+	 * @param destination
+	 */
 	public FinancialTransaction(String currency, ScheduledConcert sC, double amount, Object source, Object destination){
 		this.currency = currency;
 		this.setTransactionID();
 		this.amount = amount;
 		this.source = source;
 		this.destination = destination;
-		this.setTransactionID();
 	}
-	
+	/**
+	 * constructor
+	 * 
+	 * @param currency
+	 * @param sC
+	 * @param amount
+	 */
 	public FinancialTransaction(String currency, ScheduledConcert sC, double amount){
 		this.currency = currency;
 		this.sC = sC;
@@ -36,36 +63,36 @@ public abstract class FinancialTransaction {
 		this.amount = amount;
 		
 	}
-	/**
-	 * @return the currency
+	/**public get method that returns the currency
+	 * @return String currency
 	 */
 	public String getCurrency() {
 		return currency;
 	}
 
-	/**
-	 * @param currency the currency to set
+	/**public set method that sets the currency 
+	 * @param String currency
 	 */
 	public void setCurrency(String currency) {
 		this.currency = currency;
 	}
 
-	/**
-	 * @return the amount
+	/**public get method that returns the amount(double)
+	 * @return amount
 	 */
 	public double getAmount() {
 		return amount;
 	}
 
-	/**
-	 * @param amount the amount to set
+	/**public set method that sets the value of amount(double)
+	 * @param double amount 
 	 */
 	public void setAmount(double amount) {
 		this.amount = amount;
 	}
 
-	/**
-	 * @return the source
+	/**public get method that returns an instance of a source object
+	 * @return Object source
 	 */
 	public Object getSource() {
 		return source;
@@ -78,29 +105,41 @@ public abstract class FinancialTransaction {
 		this.source = source;
 	}
 
-	/**
+	/**public get method that returns
 	 * @return the destination
 	 */
 	public Object getDestination() {
 		return destination;
 	}
 
-	/**
-	 * @param destination the destination to set
+	/**public set method sets the value of a destination Object
+	 * @param Object destination 
 	 */
 	public void setDestination(Object destination) {
 		this.destination = destination;
 	}
-	
+	/**
+	 * public get method that returns an ID object
+	 * @return ID id
+	 */
 	public ID getTransactionID() {
 		
 		return this.id;
 	}
 
-
+    /**
+     * public set method that creates an ID object and sets it as the id for the transaction
+     * @param ID id
+     */
 	public void setTransactionID() {
 		 id = IDFactory.getID("TRN");
 	}
+	
+	/**
+	 * public method that calculates the total amount of the transaction
+	 * 
+	 * @param ArrayList<Tickets> t
+	 */
 	public void calculateAmount (ArrayList <Tickets> t){
 		for (int i = 0; i < t.size()-1;i++){
 			this.amount += t.get(i).getPrice();
@@ -109,3 +148,4 @@ public abstract class FinancialTransaction {
 	
 	
 }
+
