82b70e146768c54f01edd119065bd5ce49db0abe
wakeup9 <cho00045@algonquinlive.com>
2016-11-26 00:02:23 -0500

    add javadoc comment

--- Movement.java
+++ Movement.java
@@ -1,28 +1,44 @@
 package domain;
 
+/**
+ * This class is about movement of composition.
+ * @author Heesook
+ * @version 1.0.0 11/25/2016
+ *
+ */
 public class Movement {
-
 	
-	protected String movementName;
+	/* CONSTRUCTORS -------------------------------------------------- */
+	/**
+	 * Constructor Movement initializes variable
+	 */
+	public Movement(){ this.movementName=""; }
 	
-	public Movement(){
-		this.movementName="";
-	}
-	
-	public Movement(String m){
-		this.movementName=m;
-	}
+	/**
+	 * Constructor Movement initializes with parameter m
+	 * @param m 
+	 */
+	public Movement(String m){ this.movementName=m; }
 	
 	
+	/* ACCESSORS ----------------------------------------------------- */
+	/**
+	 * Get the movementName
+	 * @return movementName
+	 */
+	public String getMovementName(){ return movementName; }
 	
-	public String getMovementName(){
-		return movementName;
-		
-	}
-	
+	/* MODIFIERS ----------------------------------------------------- */
+	/**
+	 * Void method setMovementName sets a String variable movementName
+	 * @param movementName
+	 */
 	public void setMovementName(String movementName){
 		this.movementName = movementName;
 	}
 	
+	/* ATTRIBUTES ----------------------------------------------------- */
+	/** The attribute shows movement name as String */
+	protected String movementName;
 	
-}
+} /* end of class */
