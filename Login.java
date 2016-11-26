510c054d9f65338d72f176fd6e3cefcdf96030e4
TylerWoyiwada <woyi0001@algonquinlive.com>
2016-11-26 14:03:32 -0500

    Add files via upload
    now w/javadocs

--- Login.java
+++ Login.java
@@ -1,29 +1,36 @@
 package domain;
 
 /**
- * Login class is for the validating username/password as the user input 
- * @author Tyler, Heesook
- * @version 1.0.0 11/26/2016
+ * This class creates a login object that will check to see if the password and login are the same
+ * 
+ * @author Tyler Woyiwada
+ *
  */
 public class Login {
+/**
+ * 
+ * empty constructor
+ */
+public Login(){}
+ 
+/**
+ * attributes to be used by the class
+ * @param String username
+ * @param String password
+ */
+ 	    String username;
+	    String password;
+/**
+ * public method that checks the validity of the password and login and returns a corresponding
+ * @return String "You are logged in"
+ * @return String "Invalid username or password, please try again"
+ */
+	    public String validate(){
+	    if(username.equals(username) && password.equals(password)) 
+	        return "You are logged in";
+	    
+	   else return "Invalid username or password, please try again";
+		
+  }
+}
 
-	/* CONSTRUCTORS -------------------------------------------------- */
-	/** Constructor Login initializes variable as a String */
-	public Login(){}
-
-	String username;
-	String password;
-
-	/* NORMAL BEHAVIOR ------------------------------------------------- */
-	/**
-	 * After validating user input, it shows the confirm message.
-	 * @return String messages
-	 */
-	public String validate(){
-		if(username.equals(username) && password.equals(password)) 
-			return "You are logged in";
-
-		else return "Invalid username or password, please try again";
-
-	}
-} /* end of class : Login.java*/
