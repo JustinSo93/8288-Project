2fc35d965a3e389a93c6272ed6d8951964ee2938
JustinSo93 <justinso93@hotmail.com>
2016-11-26 12:37:29 -0500

    Add files via upload

--- StaffMember.java
+++ StaffMember.java
@@ -5,15 +5,21 @@ import java.util.ArrayList;
 public class StaffMember extends Person{
 
 	String job;
-	ArrayList<String>staffShows;
+	ArrayList<ScheduledConcert>staffShows;
 	ID staffID;
 	
-	public StaffMember(String fName, String lName, PhoneNumber phoneNumber, Address address, String emailAddress, String job, ArrayList<String>staffShows) {
-		super(fName, lName, phoneNumber, address, emailAddress);
+	public StaffMember(){
+		super();
+		this.job = "";
+		this.staffShows = new ArrayList<ScheduledConcert>();
+		this.setStaffID();
+	}
+	
+	public StaffMember(String fName, String lName, PhoneNumber phone, Address address, String emailAddress, String job, ArrayList<ScheduledConcert>staffShows) {
+		super(fName, lName, phone, address, emailAddress);
 		this.job = job;
 		this.staffShows = staffShows;
-		this.emailAddress = emailAddress;
-		this.setStaffID();; 
+		this.setStaffID(); 
 		
 		// TODO Auto-generated constructor stub
 	}
@@ -47,14 +53,14 @@ public class StaffMember extends Person{
 
 
 
-		public ArrayList<String> getStaffShows() {
+		public ArrayList<ScheduledConcert> getStaffShows() {
 			return staffShows;
 		}
 
 
 
 
-		public void setStaffShows(ArrayList<String> staffShows) {
+		public void setStaffShows(ArrayList<ScheduledConcert> staffShows) {
 			this.staffShows = staffShows;
 		}
 
@@ -63,8 +69,12 @@ public class StaffMember extends Person{
 
 		@Override
 		public String toString() {
+			String ScheduledConcertsString = new String();
+			for (int i = 0; i < staffShows.size();i++){
+				ScheduledConcertsString = ScheduledConcertsString.concat(staffShows.get(i).toString());
+			}
 		return "\nID: " + this.staffID.toString() + ", name: " + firstName + " " + lastName + ", \naddress:  " + address.toString()
-		+ ", \nphone number: " + phoneNumber + ", \njob: " + job + 
-		 ", \nShows : " + staffShows + "\n*****************************************************\n ";
+		+ ", \nphone number: " + this.getphoneNumber() + ", \njob: " + job + 
+		 ", \nShows : " + ScheduledConcertsString + "\n*****************************************************";
 }
 }
