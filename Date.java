6e7df72c7d6f662397e4f5896a3bd8710fee6b15
wakeup9 <cho00045@algonquinlive.com>
2016-11-26 08:45:46 -0500

    Add JavaDoc comment and default constructor

--- Date.java
+++ Date.java
@@ -1,80 +1,79 @@
 package domain;
 
+/**
+ * The Date class is contains all the attributes of a date such as day, month, yeare.
+ * @author Tyler, Heesook
+ * @version 1.0.0 11/26/2016
+ */
 public class Date {
 
-private int day;
-private int month;
-private int year;
+	/* CONSTRUCTORS -------------------------------------------------- */
+	/** The default constructor initializes variable */
+	public Date() {	}
 	
-	
-	
+	/**
+	 * The constructor Date initializes variables as int value of parameter x, y, z  
+	 * @param x
+	 * @param y
+	 * @param z
+	 */
 	public Date(int x, int y, int z) {
-		
 		this.day = x;
 		this.month = y;
 		this. year = z;
-	// TODO Auto-generated constructor stub
-}
+	}
 
-
+	/* ACCESSORS ----------------------------------------------------- */
+	/**
+	 * public get method that returns the value of day
+	 * @return the day
+	 */
+	public int getDay() { return day; }
 
 	/**
- * @return the day
- */
-public int getDay() {
-	return day;
-}
+	 * public get method that returns the value of month
+	 * @return the month
+	 */
+	public int getMonth() { return month; }
+	
+	/**
+	 * public get method that returns the value of year
+	 * @return the year
+	 */
+	public int getYear() { return year; }
 
+	/* MODIFIERS ----------------------------------------------------- */
+	/**
+	 * Void method setDay sets a int variable day
+	 * @param day the day to set
+	 */
+	public void setDay(int day) { this.day = day; }
+	
+	/**
+	 * Void method setMonth sets a int variable month
+	 * @param month the month to set
+	 */
+	public void setMonth(int month) { this.month = month; }
 
+	/**
+	 * Void method setYear sets a int variable year
+	 * @param year the year to set
+	 */
+	public void setYear(int year) { this.year = year; }
 
-/**
- * @param day the day to set
- */
-public void setDay(int day) {
-	this.day = day;
-}
+	/* NORMAL BEHAVIOR ------------------------------------------------- */
+	/**
+	 * Method toString returns a set of date value as a String.
+	 */
+	public String toString(){ return day+"//"+month+"//"+year; }
 
+	/* ATTRIBUTES ----------------------------------------------------- */
 
-
-/**
- * @return the month
- */
-public int getMonth() {
-	return month;
-}
-
-
-
-/**
- * @param month the month to set
- */
-public void setMonth(int month) {
-	this.month = month;
-}
-
-
-
-/**
- * @return the year
- */
-public int getYear() {
-	return year;
-}
-
-
-
-/**
- * @param year the year to set
- */
-public void setYear(int year) {
-	this.year = year;
-}
-
-public String toString(){
-	return day+"//"+month+"//"+year;
-}
-
-
-
-
-}
+	/** The attribute shows a day as a int value */
+	private int day;
+	/** The attribute shows a month as a int value */
+	private int month;
+	/** The attribute shows a year as a int value */
+	private int year;
+	
+}/* end of class : Date.java*/
