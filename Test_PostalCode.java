0efd20db9415892909901a43709e91484d9b423b
niravBaldha <niravbaldha007@gmail.com>
2016-11-26 13:36:10 -0500

    Add files via upload

--- Test_PostalCode.java
+++ Test_PostalCode.java
@@ -1,63 +1,76 @@
 package Test_p;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 import p.PostalCode;
 
+/**
+ * Test_PostalCode class is used for testing of PostalCode class. it check and
+ * test all constructor, mutator, accessors, behavior.
+ * 
+ * @author Nirav
+ *
+ */
 public class Test_PostalCode extends TestCase {
 
-	
-	public Test_PostalCode(String name) { super(name);	}
+	public Test_PostalCode(String name) {
+		super(name);
+	}
 
-	public static Test suite() { return new TestSuite(Test_PostalCode.class);		}
+	public static Test suite() {
+		return new TestSuite(Test_PostalCode.class);
+	}
 
-	protected void setUp() throws Exception { System.out.println("Test_PostalCode Begin");	}
+	protected void setUp() throws Exception {
+		System.out.println("Test_PostalCode Begin");
+	}
 
-	protected void tearDown() throws Exception { System.out.println("Test_PostalCode End");	}
-	
+	protected void tearDown() throws Exception {
+		System.out.println("Test_PostalCode End");
+	}
+
 	/**
-	 * Tests the constructor for Address	 
+	 * Tests the constructor for postalCode
 	 */
 	public void testConstructors() {
 		System.out.println("\tExecuting Test_PostalCode.testConstructors");
 		PostalCode postalCode = new PostalCode("k1g3n9");
 		assertNotNull("\t\tTest_Address.testConstructors: Address is  null", postalCode);
 
-
 	}
-	
+
+	/**
+	 * Tests the Accessors of PostalCode class
+	 */
 	public void testAccessors() {
 		System.out.println("\tExecuting Test_PostalCode.testAccessors");
 		PostalCode p1 = new PostalCode("k1g3n9");
 		assertNotNull("\t\tTest_Address.testAccessors: Address is null", p1);
 
-
-
-		
-		p1.SetPostalCode("k1g3n9");;
+		p1.SetPostalCode("k1g3n9");
+		;
 
 		assertEquals("k1g3n9", p1.GetPostalCode());
-
-		
-
 	}
-	
-	/**
-	 * Test the setters 
-	 */
 
+	/**
+	 * Test the Mutators of PostalCode Class
+	 */
 	public void testMutators() {
 		System.out.println("\tExecuting Test_PostalCode.testMutators");
 		PostalCode p2 = new PostalCode("b2n9p3");
 		assertNotNull("\t\tTest_Address.testMutators: Address is null", p2);
 
-		
 		p2.SetPostalCode("k1g3n9");
 
 		assertEquals("k1g3n9", p2.GetPostalCode());
 	}
 
+	/**
+	 * Tests the Behaviours of PostalCode class.
+	 */
 	public void testBehaviours() {
 		System.out.println("\tExecuting Test_PostalCode.testBehaviours");
 		PostalCode p3 = new PostalCode("b2n9p3");
@@ -65,12 +78,7 @@ public class Test_PostalCode extends TestCase {
 		assertEquals(true, p3.checkPostalCode("k1g3n9"));
 		assertEquals(false, p3.checkPostalCode("z1g3n9"));
 		assertEquals("b2n9p3", p3.toString());
-		
-
-		
-
 
 	}
-	
 
 }
