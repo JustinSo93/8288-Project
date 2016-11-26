d272d2a687a46bbce0b9b77d2f4ecb750ccf935a
wakeup9 <cho00045@algonquinlive.com>
2016-11-26 15:07:33 -0500

    Add Javadoc comments

--- Date.java
+++ Date.java
@@ -18,18 +18,59 @@ private int year;
 	public Date(int d, int m, int y){
 		this.day = d;
 		this.month = m;
+package domain;
+
+import javax.swing.JOptionPane;
+
+/**
+ * ConductorID class create conductorID as a String
+ * @author Tyler, Heesook, Justin
+ *
+ */
+public class Date {
+
+	/* CONSTRUCTORS -------------------------------------------------- */
+	/** The default constructor initializes variable */	
+	public Date (){
+		this.day =0;
+		this.month = 0;
+		this.year = 0;
+	}
+
+	/**
+	 * The constructor Date initializes variables as int value of parameter d, m, y  
+	 * @param d
+	 * @param m
+	 * @param y
+	 */
+	public Date(int d, int m, int y){
+		this.day = d;
+		this.month = m;
 		this.year = y;
 	}
-		/**
+
+	/* ACCESSORS ----------------------------------------------------- */
+	/**
+	 * public get method that returns the value of day
 	 * @return the day
 	 */
-	public int getDay() {
-		return day;
-	}
-	
-	
-	
+	public int getDay() { return day; }
+
 	/**
+	 * public get method that returns the value of month
+	 * @return the month
+	 */
+	public int getMonth() { return month; }
+
+	/**
+	 * public get method that returns the value of year
+	 * @return the year
+	 */
+	public int getYear() { return year; }
+
+	/* MODIFIERS ----------------------------------------------------- */
+	/**
+	 * Void method setDay sets a int variable day from 1 to 31
 	 * @param day the day to set
 	 */
 	public void setDay(int day) {
@@ -44,19 +85,9 @@ private int year;
 			this.day = day;
 		}
 	}
-	
-	
-	
+
 	/**
-	 * @return the month
-	 */
-	public int getMonth() {
-		return month;
-	}
-	
-	
-	
-	/**
+	 * Void method setMonth sets a int variable month from 1 to 12
 	 * @param month the month to set
 	 */
 	public void setMonth(int month) {
@@ -70,19 +101,9 @@ private int year;
 			this.month = month;
 		}
 	}
-	
-	
-	
+
 	/**
-	 * @return the year
-	 */
-	public int getYear() {
-		return year;
-	}
-	
-	
-	
-	/**
+	 * Void method setYear sets a int variable year from 2016 to 2100
 	 * @param year the year to set
 	 */
 	public void setYear(int year) {
@@ -95,17 +116,27 @@ private int year;
 			}
 		}
 	}
-	
-	public String toString(){
-		return day+"/"+month+"/"+year;
-	}
-	
 
+	/* NORMAL BEHAVIOR ------------------------------------------------- */
+	/** Method toString returns a set of date value as a String. */
+	public String toString(){ return day+"/"+month+"/"+year; }
 
+	/*	ENTRY POINT for STAND-ALONE OPERATION ---------------------------	*/
+	/**
+	 * The main() method print out the date set
+	 * @param  args   Standard command line parameters (arguments) as a string array.
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Date d = new Date(5,12,2014);
 		System.out.println(d.toString());
 	}
 
-}
+	/* ATTRIBUTES ----------------------------------------------------- */
+	/** The attribute shows a day as a int value */
+	private int day;
+	/** The attribute shows a month as a int value */
+	private int month;
+	/** The attribute shows a year as a int value */
+	private int year;
+
+} /*	End of CLASS:	Date.java			*/
