f1fd6987cd6d75cac06240556cdefc576231e892
wakeup9 <cho00045@algonquinlive.com>
2016-11-26 00:24:06 -0500

    add JavaDoc comments

--- Composer.java
+++ Composer.java
@@ -1,5 +1,10 @@
 package domain;
 
-public class  Composer extends Person {
+/**
+ * The Composer class is extended from Person class
+ * @author Heesook
+ *
+ */
+public class Composer extends Person {
 
-}
+} /* end of class : Composer.java*/
