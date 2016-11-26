ae4cb37f68f4fa3980475a1d2aa561838a09fb11
wakeup9 <cho00045@algonquinlive.com>
2016-11-26 00:07:48 -0500

    add Javadoc comment

--- Conductor.java
+++ Conductor.java
@@ -2,70 +2,85 @@ package domain;
 
 import java.util.ArrayList;
 
+/**
+ * The Conductor class is extended from Person class.
+ * It uses firstName, lastName, phoneNumber, address, emailAddress, 
+ * instrument, instrumentName, instrumentGroup, shows from Person class.
+ * @author Tyler, Heesook
+ * @version 1.0.0 11/25/2016
+ *
+ */
 public class Conductor extends Person{
-
-	ID conductorID;
-	String instrumentName;
-	String instrumentGroup;
-	ArrayList<String> shows;	
 	
-	
-		
-	 Conductor(String firstName, String lastName, PhoneNumber phoneNumber, Address address, String emailAddress, String instrument,
-			String instrumentGroup, ArrayList<String> shows) {
-	    	super(firstName, lastName, phoneNumber, address, emailAddress);
-			this.instrumentName =instrument;
-			this.instrumentGroup =instrumentGroup;
-			this.shows = shows;
-			this.emailAddress = emailAddress;
-			this.setConductorID();
-		// TODO Auto-generated constructor stub
+	/* CONSTRUCTORS -------------------------------------------------- */
+	/** Constructor Conductor initializes variable.*/
+	Conductor(String firstName, String lastName, PhoneNumber phoneNumber, Address address, 
+			String emailAddress, String instrument, String instrumentGroup, 
+			ArrayList<String> shows) {
+		super(firstName, lastName, phoneNumber, address, emailAddress);
+		this.instrumentName =instrument;
+		this.instrumentGroup =instrumentGroup;
+		this.shows = shows;
+		this.emailAddress = emailAddress;
+		this.setConductorID();	
 	}
 
-		
+	/* ACCESSORS ----------------------------------------------------- */
+	/**
+	 * This method gets conductorID
+	 * @return conductorID
+	 */
+	public ID getConductorID() { return conductorID; }
 
-
-
-
-
-
-
-
-
-
-
-		public ID getConductorID() {
-			
-			return conductorID;
-		}
-
-
-		public void setConductorID() {
-			 conductorID = IDFactory.getID("CON");
-		}
+	/**
+	 * This method gets InstrumentGrup
+	 * @return instrumentGroup
+	 */
+	public String getInstrumentGroup() { return instrumentGroup; }
 	
-		public String getInstrumentGroup() {
-			return instrumentGroup;
-		}
+	/** 
+	 * This method gets InstrumentName
+	 * @return instrumentName
+	 */
+	public String getInstrumentName() { return instrumentName; }
+	
+	/* MODIFIERS ----------------------------------------------------- */
+	/** Void method setConductorID sets a conductorID */
+	public void setConductorID() { conductorID = IDFactory.getID("CON"); }
 
+	/**
+	 * Void method setInstrumentGroup sets a String variable instrumentGroup
+	 * @param instrumentGroup
+	 */
+	public void setInstrumentGroup(String instrumentGroup) { this.instrumentGroup = instrumentGroup; }
 
-		public void setInstrumentGroup(String instrumentGroup) {
-			this.instrumentGroup = instrumentGroup;
-		}
-
-
-		public String getInstrumentName() {
-			return instrumentName;
-		}
-
-
-		public void setInstrumentName(String instrumentName) {
-			this.instrumentName = instrumentName;
-		}
-		@Override
-		public String toString() {
+	/**
+	 * Void method setInstrumentName sets a String variable instrumentName
+	 * @param instrumentName
+	 */
+	public void setInstrumentName(String instrumentName) { this.instrumentName = instrumentName; }
+	
+	/* HELPER METHODS ----------------------------------------------------- */
+	/**
+	 * This method toSting sets the display format for the attributes
+	 */
+	@Override
+	public String toString() {
 		return "\nID: " + this.conductorID.toString() + ", name: " + firstName + " " + lastName + ", \naddress:  " + address.toString()
 		+ ", \nphone number: " + phoneNumber + ", \nInstrument group: " + instrumentGroup
 		+ ", Instrument name =" + instrumentName + "\nConcerts: " +shows + "\n*****************************************************\n ";
-}
+	}
+	
+	/* ATTRIBUTES ----------------------------------------------------- */
+	/** The attribute shows conductID as ID */
+	ID conductorID;
+	
+	/** The attribute shows instrument name as String */
+	String instrumentName;
+	
+	/** The attribute shows insturment group as String  */
+	String instrumentGroup;
+	
+	/** The attribute shows shows as ArrayList<String>*/
+	ArrayList<String> shows;
 }
