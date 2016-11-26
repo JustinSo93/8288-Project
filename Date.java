a41ecb81bc7ca947c8773247ac550757c014d989
JustinSo93 <justinso93@hotmail.com>
2016-11-26 13:33:52 -0500

    Add files via upload

--- Date.java
+++ Date.java
@@ -1,79 +1,111 @@
 package domain;
 
-/**
- * The Date class is contains all the attributes of a date such as day, month, yeare.
- * @author Tyler, Heesook
- * @version 1.0.0 11/26/2016
- */
+import javax.swing.JOptionPane;
+
 public class Date {
 
-	/* CONSTRUCTORS -------------------------------------------------- */
-	/** The default constructor initializes variable */
-	public Date() {	}
+private int day;
+private int month;
+private int year;
 	
-	/**
-	 * The constructor Date initializes variables as int value of parameter x, y, z  
-	 * @param x
-	 * @param y
-	 * @param z
-	 */
-	public Date(int x, int y, int z) {
-		this.day = x;
-		this.month = y;
-		this. year = z;
+	
+	public Date (){
+		this.day =0;
+		this.month = 0;
+		this.year = 0;
 	}
-
-	/* ACCESSORS ----------------------------------------------------- */
-	/**
-	 * public get method that returns the value of day
+	
+	public Date(int d, int m, int y){
+		this.day = d;
+		this.month = m;
+		this.year = y;
+	}
+		/**
 	 * @return the day
 	 */
-	public int getDay() { return day; }
-
-	/**
-	 * public get method that returns the value of month
-	 * @return the month
-	 */
-	public int getMonth() { return month; }
+	public int getDay() {
+		return day;
+	}
+	
+	
 	
 	/**
-	 * public get method that returns the value of year
-	 * @return the year
-	 */
-	public int getYear() { return year; }
-
-	/* MODIFIERS ----------------------------------------------------- */
-	/**
-	 * Void method setDay sets a int variable day
 	 * @param day the day to set
 	 */
-	public void setDay(int day) { this.day = day; }
+	public void setDay(int day) {
+		if (day <31 && day <=0){
+			this.day = day;
+		}
+		else{
+			while(day>31 || day<=0){
+				String s = JOptionPane.showInputDialog("Error: Enter the day");
+				day = Integer.parseInt(s);
+			}
+			this.day = day;
+		}
+	}
+	
+	
 	
 	/**
-	 * Void method setMonth sets a int variable month
+	 * @return the month
+	 */
+	public int getMonth() {
+		return month;
+	}
+	
+	
+	
+	/**
 	 * @param month the month to set
 	 */
-	public void setMonth(int month) { this.month = month; }
-
+	public void setMonth(int month) {
+		if (month>0 && month<13){
+			this.month = month;
+		}else{
+			while(month<0 || month>13){
+				String s = JOptionPane.showInputDialog("Enter a month between 1 and 12");
+				month = Integer.parseInt(s);
+			}
+			this.month = month;
+		}
+	}
+	
+	
+	
 	/**
-	 * Void method setYear sets a int variable year
+	 * @return the year
+	 */
+	public int getYear() {
+		return year;
+	}
+	
+	
+	
+	/**
 	 * @param year the year to set
 	 */
-	public void setYear(int year) { this.year = year; }
-
-	/* NORMAL BEHAVIOR ------------------------------------------------- */
-	/**
-	 * Method toString returns a set of date value as a String.
-	 */
-	public String toString(){ return day+"//"+month+"//"+year; }
-
-	/* ATTRIBUTES ----------------------------------------------------- */
-
-	/** The attribute shows a day as a int value */
-	private int day;
-	/** The attribute shows a month as a int value */
-	private int month;
-	/** The attribute shows a year as a int value */
-	private int year;
+	public void setYear(int year) {
+		if (year>1999 && year<2100){
+			this.year = year;
+		}else{
+			while(year<2016 || year>2100){
+				String s = JOptionPane.showInputDialog("Enter a year between 2016 and 2100");
+				year = Integer.parseInt(s);
+			}
+		}
+	}
 	
-}/* end of class : Date.java*/
+	public String toString(){
+		return day+"/"+month+"/"+year;
+	}
+	
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date d = new Date(5,12,2014);
+		System.out.println(d.toString());
+	}
+
+}
