318c812ac72160aed0d22174f072c65a16d00a94
TylerWoyiwada <woyi0001@algonquinlive.com>
2016-11-26 13:16:38 -0500

    Add files via upload
    with javadoc

--- CustomerID.java
+++ CustomerID.java
@@ -1,19 +1,36 @@
 package domain;
-
+/**
+ * This class creates a customer ID code 
+ * 
+ * @author Tyler Woyiwada
+ *
+ */
 public class CustomerID implements ID {
-
+/**
+ * attributes to be used within the class
+ * @param String musCode
+ * @param int musCount
+ */
 public static int musCount;
 	
 	
 	String musCode = "CUS";
 
 
-
+/**
+ * public get method that returns musCode to the toString method
+ * @param String musCode
+ */
 	@Override
 	public String createID() {
 				return musCode.toString();
 				}
-	
+/**
+ * public toString method that returns a String version of the CustomerID object
+ * @param String musCode
+ * @param int musCount
+ * @return musCode + musCount
+ */
 	@Override 
 	public String toString(){
 		musCount ++;
