a97c3ebe608ae9d9fc6ef46e652627d6bc3bfe16
wakeup9 <cho00045@algonquinlive.com>
2016-11-26 00:15:35 -0500

    Add JavaDoc comment

--- ID.java
+++ ID.java
@@ -1,6 +1,14 @@
 package domain;
 
+/**
+ * This is an interface defines the ID.
+ * @author Tyler, Heesook
+ * @version 1.0.0 11/25/2016
+ *
+ */
 public interface ID{
+	
 	String createID();
 	String toString();
-}
+	
+} /* end of class : ID.java*/
