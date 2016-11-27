cea7fefbe091c0f0bd22aee40d84c9ff5d8b5608
TylerWoyiwada <woyi0001@algonquinlive.com>
2016-11-26 20:26:03 -0500

    w/javadocs
    w/javadocs

--- Purchase.java
+++ Purchase.java
@@ -1,19 +1,39 @@
 package domain;
 
 import java.util.ArrayList;
+/**
+ * This class creates a Purchase object that extends FinancialTransaction
+ * 
+ * @author Tyler Woyiwada, Justin So
+ *
+ */
 
 public class Purchase extends FinancialTransaction{
 	
+	/**
+	 * empty Constructor
+	 */
 	
 	public Purchase(){
 		super();
 	}
 	
-	
+	/**
+	 * Parameterized constructor
+	 * @param String currency
+	 * @param ScheduledConcert sC
+	 * @param double amount
+	 */
 	public Purchase(String currency, ScheduledConcert sC, double amount) {
 		super(currency, sC, amount);
 	}
 	
+	/**
+	 * copy constructor
+	 * 
+	 * @param String currency
+	 * @param ArrayList<Tickets>t
+	 */
 	public Purchase(String currency, ArrayList<Tickets> t){
 		this.currency = currency;
 		this.sC = t.get(0).getScheduledConcert();
@@ -23,7 +43,10 @@ public class Purchase extends FinancialTransaction{
 		
 	}
 
-
+/**
+ * public set method sets the source of the purchase and checks to see if it the source is a customer object
+ * @param Object source
+ */
 	@Override
 	public void setSource(Object source) throws ClassCastException{
 		try{
@@ -34,6 +57,10 @@ public class Purchase extends FinancialTransaction{
 		}
 	}
 	
+/**
+ * public get method that returns the source and checks to see if it is from a Customer object
+ * @return Object source
+ */
 	@Override
 	public Object getSource () throws ClassCastException{
 		try{
@@ -44,7 +71,11 @@ public class Purchase extends FinancialTransaction{
 		}
 		return this.source;
 	}
-	
+/**
+ * public set method that sets the value of the destination and checks to see if the destination is a ScheduledConcert object
+ * @param Object d
+ *
+ */
 	@Override
 	public void setDestination(Object d) throws ClassCastException{
 		try{
@@ -54,7 +85,11 @@ public class Purchase extends FinancialTransaction{
 			e.printStackTrace();
 		}
 	}
-	
+
+/**
+ * public get method that returns a destinations and checks to see if it is a ScheduledConcert object
+ * @return Object destination
+ */
 	@Override
 	public Object getDestination() throws ClassCastException{
 		try{
