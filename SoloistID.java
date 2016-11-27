0453426301af626ca4746d74a3ccb7a961e176bd
TylerWoyiwada <woyi0001@algonquinlive.com>
2016-11-26 21:21:43 -0500

    w/javadocs

--- SoloistID.java
+++ SoloistID.java
@@ -1,19 +1,38 @@
-
 package domain;
 
+/**
+ * This class creates a Soloist ID code 
+ * 
+ * @author Tyler Woyiwada
+ *
+ */
+
 public class SoloistID implements ID{
+	/**
+	 * attributes to be used within the class
+	 * @param String soloCode
+	 * @param int soloCount
+	 */
+	public static int soloCount = 0;
+	String soloCode = "SOL";
+	/**
+	 * public get method that returns soloCode to the toString method
+	 * @param String musCode
+	 */
+	@Override
+	public String createID() {
 
-		public static int soloCount = 0;
-			String soloCode = "SOL";
-
-			@Override
-			public String createID() {
-				soloCount++;
-						return soloCode.toString();
-			}
-			@Override 
-			public String toString(){
-				
-				return soloCode+1 + soloCount;
-			}
-		}
+		return soloCode.toString();
+	}
+	/**
+	 * public toString method that returns a String version of the SoloistID object
+	 * @param String soloCode
+	 * @param int musCount
+	 * @return soloCode + soloCount
+	 */
+	@Override 
+	public String toString(){
+		soloCount++;
+		return soloCode + soloCount;
+	}
+}
