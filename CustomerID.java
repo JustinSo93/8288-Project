3015a5c689e26517374e16fc012f99e7245c69b7
Jessica Stratton <stra0157@algonquinlive.com>
2016-11-27 21:24:22 -0500

    Add files via upload

--- CustomerID.java
+++ CustomerID.java
@@ -1,4 +1,5 @@
 package domain;
+
 /**
  * This class creates a customer ID code 
  * 
@@ -6,34 +7,45 @@ package domain;
  *
  */
 public class CustomerID implements ID {
-/**
- * attributes to be used within the class
- * @param String musCode
- * @param int musCount
- */
-public static int musCount;
-	
-	
-	String musCode = "CUS";
 
+	/* CONSTRUCTORS	--------------------------------------------------	*/
+	/**
+	 * constructor that increments the ID code
+	 */
+	public CustomerID(){
+		this.id = musCount++;
 
-/**
- * public get method that returns musCode to the toString method
- * @param String musCode
- */
+	}
+
+	/*	NORMAL BEHAVIOR -------------------------------------------------	*/
+	/**
+	 * public get method that returns musCode to the toString method
+	 * @param String musCode
+	 */
 	@Override
 	public String createID() {
-				return musCode.toString();
-				}
-/**
- * public toString method that returns a String version of the CustomerID object
- * @param String musCode
- * @param int musCount
- * @return musCode + musCount
- */
+		return musCode.toString();
+	}
+	/**
+	 * public toString method that returns a String version of the CustomerID object
+	 * @param String musCode
+	 * @param int id
+	 * @return musCode + id
+	 */
 	@Override 
 	public String toString(){
-		musCount ++;
-		return musCode + musCount;
+	
+		return musCode + id;
 	}
+
+	/* ATTRIBUTES	--------------------------------------------------	*/	
+	/**
+	 * attributes to be used within the class
+	 * @param String musCode
+	 * @param int musCount
+	 * @param int id
+	 */
+	private static int musCount;
+	public String musCode = "CUS";
+	private int id;
 }
