6dee779f27ad3a7fe00f075f2eb9f2d6dc4491ae
JustinSo93 <justinso93@hotmail.com>
2016-11-27 19:11:36 -0500

    Update Login.java

--- Login.java
+++ Login.java
@@ -7,32 +7,41 @@ package domain;
  *
  */
 
-/**
- * attributes to be used by the class
- * @param String username
- * @param String password
- */
- 	   protected String username;
-	   protected String password;
+
 
 public class Login {
-/**
- * 
- * empty constructor
- */
-public Login(){}
- 
-/**
- * public method that checks the validity of the password and login and returns a corresponding
- * @return String "You are logged in"
- * @return String "Invalid username or password, please try again"
- */
-	    public String validate(){
-	    if(username.equals(username) && password.equals(password)) 
-	        return "You are logged in";
-	    
-	   else return "Invalid username or password, please try again";
+	
+	
+	/* CONSTRUCTORS -------------------------------------------------- */	
+		/**
+		 * 
+		 * empty constructor
+		 */
+		public Login(){}
+	/*	NORMAL BEHAVIOR -------------------------------------------------	*/
+
+		/**
+		 * public method that checks the validity of the password and login and returns a corresponding
+		 * @return String "You are logged in"
+		 * @return String "Invalid username or password, please try again"
+		 */
+		    public String validate(){
+		    if(username.equals(username) && password.equals(password)) 
+			return "You are logged in";
+
+		   else return "Invalid username or password, please try again";
+		    
+
 		
-  }
+  		}
+			/* ATTRIBUTES ----------------------------------------------------- */
+		    
+			/**
+			 * attributes to be used by the class
+			 * @param String username
+			 * @param String password
+			 */
+		 	   protected String username;
+			   protected String password;    
 }
 
