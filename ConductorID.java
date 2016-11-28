d69beec6e17b586c2c6ceb24727dccc399691fff
JustinSo93 <justinso93@hotmail.com>
2016-11-27 20:05:36 -0500

    Update ConductorID.java

--- ConductorID.java
+++ ConductorID.java
@@ -6,41 +6,45 @@ package domain;
  *
  */
 public class ConductorID implements ID{
+	
+/* CONSTRUCTORS	--------------------------------------------------	*/
+/**
+ * constructor that increments the ID code
+ */
+public ConductorID(){
+this.id = conCount++;
+
+}
+	/*	NORMAL BEHAVIOR -------------------------------------------------	*/
+	/**
+	 * get method for the musCode
+	 * @return musCode.toString
+	 * 
+	 */
+	@Override
+	public String createID() {
+		    
+				return conCode.toString();
+				}
+	/* HELPER METHODS	--------------------------------------------------	*/
+	/**
+	 * toString method for the ID code.
+	 * @return musCount + musCode toString.
+	 */
+	@Override 
+	public String toString(){
+		
+		return conCode + id;
+	}
+	
+/* ATTRIBUTES	-----------------------------------------------------	*/
 /**Variables that form the ID Key.
  * @param public int musCount
  * @param public String musCode
  * 
  */
 	private int id;
-	private String musCode = "CON";
-    private static int musCount;
-/**
- * constructor that increments the ID code
- */
-public ConductorID(){
-this.id = musCount++;
-
-}
-	
-/**
- * get method for the musCode
- * @return musCode.toString
- * 
- */
-	@Override
-	public String createID() {
-		    
-				return musCode.toString();
-				}
-/**
- * toString method for the ID code.
- * @return musCount + musCode toString.
- */
-	@Override 
-	public String toString(){
-		
-		return musCode + id;
-	}
-
+	private String conCode = "CON";
+    	private static int conCount;
 	
 }
